972b842f7d20d0595f09e5f5129cf17df7df846f
HugoPadilla25 <126732689+HugoPadilla25@users.noreply.github.com>
2023-07-31 22:34:37 -0600

    Text modify

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello world')
-});
+app.get('/', (req, res) => res.send('<h1>Express con HTML!</h1>'));
 
-app.listen(3000, () => {
-    console.log('Server on port 3000')
-});
+app.listen(3000);
+console.log('Server on port 3000')
